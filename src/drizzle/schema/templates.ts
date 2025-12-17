a9669ea7121b290f8da86391e537726a2051bdb8
Clement Delphin <clementcla7@gmail.com>
2025-12-17 15:08:34 +0530

    add templates table schema with user relation

---
+++ src/drizzle/schema/templates.ts
@@ -0,0 +1,12 @@
+import { integer, jsonb, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { users } from "./users.js";
+
+export const templates = pgTable('templates', {
+    id: serial('id').primaryKey(),
+    userId: integer('user_id').references(() => users.id, { onDelete: "cascade" }),
+    name: text('name').notNull(),
+    data: jsonb('data').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull()
+})
+
